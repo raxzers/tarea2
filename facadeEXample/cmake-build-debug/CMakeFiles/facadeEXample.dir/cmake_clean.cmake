e24647d233a50d4d41f9a1b631a7795ab3443890
jhon0596 <jrodriguez050796@gmail.com>
2019-09-12 19:58:47 -0600

    Ejemplo de facade
    forma de llamar subsistemas con un metodo

---
+++ facadeEXample/cmake-build-debug/CMakeFiles/facadeEXample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facadeEXample.dir/main.cpp.o"
+  "facadeEXample.pdb"
+  "facadeEXample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facadeEXample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
